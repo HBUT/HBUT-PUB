63178500293770eb7e41e5286050d395868ad0ce
JIZHUA <jizhua@fenjiandan.com>
2018-07-25 16:00:18 +0800

    add vue-cookie

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import VueCookie from 'vue-cookie'
 import App from './App'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(VueResource)
+Vue.use(VueCookie);
 
 /* eslint-disable no-new */
 new Vue({
