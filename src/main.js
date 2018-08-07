6f784ea7368d80c4c00b35b1a2fcbf126b429f59
JIZHUA <jizhua@fenjiandan.com>
2018-08-07 21:04:06 +0800

    change request solution

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import VueCookie from 'vue-cookie'
+import axios from 'axios'
+
 import App from './App'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-Vue.use(VueResource)
 Vue.use(VueCookie)
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
@@ -17,9 +18,5 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>',
-  http: {
-    root: '/apiv1',
-    headers: {}
-  }
+  template: '<App/>'
 })
