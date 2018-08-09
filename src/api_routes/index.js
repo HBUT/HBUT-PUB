fb15516a1c3085ebaa62c2eea4595f7aa40f6dba
JIZHUA <jizhua@fenjiandan.com>
2018-08-09 19:44:36 +0800

    add api routes for practice info

--- src/api_routes/index.js
+++ src/api_routes/index.js
@@ -1,2 +1,6 @@
-export const LOGIN = `/user/login` // POST
-export const REGISTER = `/user/register` // POST
+export const LOGIN = `/user/login` // POST登录
+export const REGISTER = `/user/register` // POST注册
+
+export const LIST_PRACTICE_BY_TYPE = `/practice/listPracticeByType` // GET获取内推列表
+export const GET_PRACTICE_BY_ID = `/practice/getPracticeById` // GET获取单个内推信息
+export const SAVE_PRACTICE = `/practice/savePractice` // POST提交内推信息
