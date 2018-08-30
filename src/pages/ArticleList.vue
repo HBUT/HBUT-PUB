9c56e479b634cadfc813bacf5381a7a2fbf91177
JIZHUA <jizhua@fenjiandan.com>
2018-08-30 16:19:44 +0800

    add a feature of article list

---
+++ src/pages/ArticleList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'ArticalList',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../assets/scss/articlelist";
+</style>
