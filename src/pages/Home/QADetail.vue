acca8f1929dadfb0b0ea870b3d303fa192f13dd7
JIZHUA <jizhua@fenjiandan.com>
2018-09-08 16:21:27 +0800

    feat: qa

--- src/pages/Home/QADetail.vue
+++ src/pages/Home/QADetail.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="qa-detail">
-
+    <div class="tc"></div>
+    <div class="c-w">
+      <div class="content"></div>
+      <div class="fr">回复</div>
+    </div>
+    <ul>
+      <li>
+        <div></div>
+        <div class="fr">回复</div>
+      </li>
+    </ul>
   </div>
 </template>
 
--- src/pages/Home/QAForm.vue
+++ src/pages/Home/QAForm.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <input type="text" v-model="title">
-    <textarea v-model="content" maxlength="300"></textarea>
+  <div class="qa-form">
+    <template>
+      <input type="text" class="f14" v-model="title" placeholder="请输入主题">
+      <textarea v-model="content" class="f14" maxlength="420" placeholder="请输入问题内容"></textarea>
+    </template>
   </div>
 </template>
 
@@ -18,5 +20,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import "../../assets/scss/qaform";
+  @import "../../assets/scss/qaform";
 </style>
